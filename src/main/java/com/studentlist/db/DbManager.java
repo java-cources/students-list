99456a12f9fb462b02e908ea8eaa65354f759e46
Myltykbayev Alibek <myltyk133@gmail.com>
2026-03-01 20:43:51 +0500

    feat(main): Добавил подсвечивание строки студента c оценкой F

--- src/main/java/com/studentlist/db/DbManager.java
+++ src/main/java/com/studentlist/db/DbManager.java
@@ -17,7 +17,7 @@ public class DbManager {
         students.add(new Student(1L, "Alibek", "Myltykbayev", 95, "A"));
         students.add(new Student(2L, "Rasul", "Myltykbayev", 88, "B+"));
         students.add(new Student(3L, "Aruzhan", "Saparova", 76, "C+"));
-        students.add(new Student(4L, "Dias", "Kenzhebayev", 91, "A-"));
+        students.add(new Student(4L, "Dias", "Kenzhebayev", 49, "F"));
         students.add(new Student(5L, "Dana", "Tulegenova", 84, "B"));
 
         long maxId = students.stream()
