33121e8b0507026d82751d25a99cf4061d313c6e
Myltykbayev Alibek <myltyk133@gmail.com>
2026-03-01 20:15:51 +0500

    feat(main): Функционал добавления студента

--- src/main/java/com/studentlist/controller/MainController.java
+++ src/main/java/com/studentlist/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.studentlist.controller;
 
 import com.studentlist.db.DbManager;
+import com.studentlist.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -13,5 +15,12 @@ public class MainController {
         model.addAttribute("students", DbManager.getStudents());
 
         return "index";
+    }
+
+    @PostMapping("/add-student")
+    public String save(Student student) {
+        DbManager.addStudent(student);
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/studentlist/db/DbManager.java
+++ src/main/java/com/studentlist/db/DbManager.java
@@ -4,11 +4,14 @@ import com.studentlist.model.Student;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class DbManager {
 
     @Getter
-    private static ArrayList<Student> students = new ArrayList<>();
+    private static final ArrayList<Student> students = new ArrayList<>();
+
+    private static final AtomicLong idGenerator = new AtomicLong();
 
     static {
         students.add(new Student(1L, "Alibek", "Myltykbayev", 95, "A"));
@@ -16,5 +19,31 @@ public class DbManager {
         students.add(new Student(3L, "Aruzhan", "Saparova", 76, "C+"));
         students.add(new Student(4L, "Dias", "Kenzhebayev", 91, "A-"));
         students.add(new Student(5L, "Dana", "Tulegenova", 84, "B"));
+
+        long maxId = students.stream()
+                .mapToLong(Student::getId)
+                .max()
+                .orElse(0);
+
+        idGenerator.set(maxId);
+    }
+
+    /**
+     * Добавляем нового студента в память, перед тем как вычислим его Mark и присвоим ID
+     * @param student
+     */
+    public static void addStudent(Student student) {
+        if (student.getExam() >= 90) {
+            student.setMark("A");
+        } else if (student.getExam() >= 75) {
+            student.setMark("B");
+        } else if (student.getExam() >= 60) {
+            student.setMark("C");
+        } else {
+            student.setMark("F");
+        }
+
+        student.setId(idGenerator.incrementAndGet());
+        students.add(student);
     }
 }
